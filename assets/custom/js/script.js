f156bfa721040eee4c1e07591523f727d1cb8884
CyberCell-Viit <cybercell@viit.ac.in>
2023-04-08 12:07:25 +0530

    Added

---
+++ assets/custom/js/script.js
@@ -0,0 +1,141 @@
+var particles = 100;
+if (screen.width < 500) {
+    particles = 60
+}
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": particles,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ff0000"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ff0000",
+            "opacity": 0.8,
+            "width": 1.5
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true,
+    "config_demo": {
+        "hide_card": false,
+        "background_color": "#000000",
+        "background_image": "",
+        "background_position": "50% 50%",
+        "background_repeat": "no-repeat",
+        "background_size": "cover"
+    }
+});
+
+// function countdown() {
+//     var countDownDate = new Date("Mar 31, 2023 10:30:00").getTime();
+//     var x = setInterval(function() {
+//         var now = new Date().getTime();
+//         var distance = countDownDate - now;
+//         if (distance > 0) {
+//             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//             var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//             var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+//         } else {
+//             days = hours = minutes = seconds = 0
+//         }
+//         document.getElementById("time-days").innerHTML = days;
+//         document.getElementById("time-hrs").innerHTML = hours;
+//         document.getElementById("time-min").innerHTML = minutes;
+//         document.getElementById("time-sec").innerHTML = seconds
+//     }, 1000)
+// }
+// countdown();
